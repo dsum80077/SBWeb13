23f8461021c3bb84fb9dd72403a9abd83f4065a3
ssanjay8 <ssanjay8@host.docker.internal>
2021-11-24 15:00:02 +0530

    Updated

---
+++ SBWeb13/src/main/java/com/example/demo/controller/Student.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+public class Student {
+	private int roll;
+	private String sname;
+	public int getRoll() {
+		return roll;
+	}
+	public void setRoll(int roll) {
+		this.roll = roll;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	
+
+}
