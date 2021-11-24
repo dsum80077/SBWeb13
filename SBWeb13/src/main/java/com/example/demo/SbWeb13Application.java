54fcb7a9400368bc57162802d87629abb3df25a7
ssanjay8 <ssanjay8@host.docker.internal>
2021-11-24 14:05:54 +0530

    this is github

---
+++ SBWeb13/src/main/java/com/example/demo/SbWeb13Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbWeb13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbWeb13Application.class, args);
+	}
+
+}
